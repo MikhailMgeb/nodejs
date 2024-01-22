333cbc29bf2898ff3ef03c284ec88ff41d6f9356
Michael Mgebrishvili <mmd920802@gmail.com>
2024-01-22 14:20:08 +0300

    refactor third task

--- bread-URL/index.js
+++ bread-URL/index.js
@@ -1,10 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    const url = req.url;
+    const path = req.url.split('/').filter((item) => item !== '');
 
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.write('<a href="/razminochnyje/node-js">Главная / Разминочные задания / Node.JS</a>');
+
+    let link = '';
+
+    res.write(`/<a class="links" href="http://localhost:3000">http://localhost:3000</a>`);
+
+    for (const segmentPath of path) {
+      link += `/${segmentPath}`;
+      res.write(`/<a class="links" href="http://localhost:3000${link}">${segmentPath}</a>`);
+    }
 
     res.end();
 })
